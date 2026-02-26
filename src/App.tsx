30729ca483f05b77e3f2556c161491020e78fc5b
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2026-02-27 06:18:29 +0700

    aaaaa body bang bang bang bang bang

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,71 @@
+import { motion } from "motion/react";
+
 function App() {
+  const headingText = "æspa";
+  const members = [
+    'KARINA',
+    'GISELLE',
+    'WINTER',
+    'NINGNING',
+  ];
   return (
     <>
-      <section className="bg-[#070fc1] w-full h-screen relative overflow-hidden">
-        <div className="w-full flex flex-row gap-5 justify-center font-bold text-xl text-black bg-[#f4f015] py-5">
-          <h1>æspa</h1>
-          <h1>æspa</h1>
-          <h1>æspa</h1>
+      <motion.section className="bg-[#070fc1] w-full h-screen relative overflow-hidden"
+          initial={{
+            filter: "blur(10px)",
+          }}
+          whileInView={{
+            filter: "blur(0px)",
+          }}
+          transition={{
+            filter: {
+              duration: 1,
+            }
+          }}>
+        <div className="w-full flex flex-row gap-5 justify-center font-bold text-xl text-[#f4f015] bg-none py-5">
+          <img src="images/aespa-icon.png" className="w-18 absolute left-5 top-5"/>
+          <h1 className="cursor-pointer">ABOUT</h1>
+          <h1 className="cursor-pointer">MEMBERS</h1>
+          <h1 className="cursor-pointer">KWANGYA</h1>
         </div>
         <div className="flex justify-center items-start">
-          <img src="images/aespa.png" className="absolute bottom-0 w-8/12 mask-b-from-50%"/>
-          <h1 className="text-[#f4f015] absolute bottom-32 text-9xl font-bold z-10">æspa</h1>
+          <motion.img src="images/aespa.png" className="absolute bottom-0 w-8/12 mask-b-from-50%" initial={{
+            opacity: 0,
+            scale: 1.1,
+            y: 50,
+            filter: "blur(10px)",
+          }}
+          whileInView={{
+            opacity: 1,
+            scale: 1,
+            y: 0,
+            filter: "blur(0px)",
+          }}
+          transition={{
+            opacity : {
+              duration : 1.2
+            },
+            scale: {
+              duration: 0.3,
+              delay: 0.5,
+            },
+            y: {
+              duration: 0.5,
+            },
+            filter: {
+              duration: 0.5
+            }
+          }}/>
+          <h1 className="text-[#f4f015] absolute bottom-32 text-9xl font-bold z-10">{headingText}</h1>
           <div className="absolute bottom-14 text-[#f4f015] text-3xl font-bold z-10 flex flex-row gap-5">
-            <h1 className="">KARINA</h1>
-            <h1 className="">GISELLE</h1>
-            <h1 className="">WINTER</h1>
-            <h1 className="">NINGNING</h1>
+            {members.map(member => (
+              <span key={member}>{member}</span>
+            ))}
           </div>
         </div>
+      </motion.section>
+      <section className="bg-black min-h-screen">
+
       </section>
     </>
   );
