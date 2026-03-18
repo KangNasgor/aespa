d6c77ec02df51ceda8fe175727864d367beb6337
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2026-03-18 12:43:26 +0700

    hvjdfsedfvhjksedfvhj

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { motion } from "motion/react";
-import { useContext, useState } from "react";
 import Navbar from "./components/Navbar";
 import { useTheme } from "./context/ThemeContext";
 
@@ -29,7 +28,7 @@ function App() {
   const icon = theme === "dark" ? "images/aespa-dirtywork-icon.png" : "images/aespa-richman-icon.png";
   return (
     <>
-      <motion.section id="hero-section" data-theme={theme} className="bg-[#070fc1] dark:bg-black w-full h-screen relative overflow-hidden"
+      <motion.section id="hero-section" data-theme={theme} className="bg-[#070fc1] dark:bg-black w-full h-screen relative"
         initial={{
           filter: "blur(10px)",
         }}
@@ -41,45 +40,52 @@ function App() {
             duration: 1,
           }
         }}>
-          <Navbar icon={icon}/>
-          <div className="flex justify-center items-start">
-            <motion.img src="images/aespa-dirtywork.png" className="absolute bottom-0 w-8/12 mask-b-from-50%" initial={{
-              opacity: 0,
-              scale: 1.1,
-              y: 50,
-              filter: "blur(10px)",
+        <Navbar icon={icon} />
+        <div className="flex justify-center items-start">
+          <motion.img src="images/aespa-dirtywork.png" className="absolute bottom-0 w-8/12 mask-b-from-50%" initial={{
+            opacity: 0,
+            scale: 1.1,
+            y: 50,
+            filter: "blur(10px)",
+          }}
+            whileInView={{
+              opacity: 1,
+              scale: 1,
+              y: 0,
+              filter: "blur(0px)",
             }}
-              whileInView={{
-                opacity: 1,
-                scale: 1,
-                y: 0,
-                filter: "blur(0px)",
-              }}
-              transition={{
-                opacity: {
-                  duration: 1.2
-                },
-                scale: {
-                  duration: 0.3,
-                  delay: 0.5,
-                },
-                y: {
-                  duration: 0.5,
-                },
-                filter: {
-                  duration: 0.5
-                }
-              }} />
-            <h1 className="text-[#f4f015] dark:text-white absolute bottom-32 text-9xl font-bold z-10">{headingText}</h1>
-            <div className="absolute bottom-16 text-[#f4f015] dark:text-white text-3xl font-bold z-10 flex flex-row gap-10">
-              {members.map(member => (
-                <span key={member.name}>{member.name}</span>
-              ))}
-            </div>
+            transition={{
+              opacity: {
+                duration: 1.2
+              },
+              scale: {
+                duration: 0.3,
+                delay: 0.5,
+              },
+              y: {
+                duration: 0.5,
+              },
+              filter: {
+                duration: 0.5
+              }
+            }} />
+          <h1 className="text-[#f4f015] dark:text-white absolute bottom-32 text-9xl font-bold z-10">{headingText}</h1>
+          <div className="absolute bottom-16 text-[#f4f015] dark:text-white text-3xl font-bold z-10 flex flex-row gap-10">
+            {members.map(member => (
+              <span key={member.name}>{member.name}</span>
+            ))}
           </div>
+        </div>
       </motion.section>
-      <section className="bg-black min-h-screen">
-
+      <section className="bg-[#070fc1] dark:bg-black min-h-screen" data-theme={theme}>
+            <div className="flex flex-row justify-between items-start px-10 pt-36">
+                <div className="w-6/12">
+                    <h1 className="text-[#f4f015] dark:text-white font-bold text-5xl">The æ concept that changed everything</h1>
+                </div>
+                <div className="w-6/12">
+                    
+                </div>
+            </div>
       </section>
     </>
   );
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useState, type ReactNode, useContext } from "react";
 
 type Theme = "light" | "dark"
-interface ThemeContextType {
+interface ThemeContextType { // this type is to make the context provides values it need
     theme : Theme,
     changeTheme : () => void,
 }
@@ -14,13 +14,13 @@ export function ThemeProvider ({ children } : { children : ReactNode}){
     }
 
     return(
-        <ThemeContext.Provider value={{ theme, changeTheme}}>
+        <ThemeContext.Provider value={{ theme, changeTheme }}>
             {children}
         </ThemeContext.Provider>
     )
 }
 
-export const useTheme = () => {
+export const useTheme = () => { // custom hook to make this context usage easier
     const context = useContext(ThemeContext);
     if(!context) throw new Error("useTheme must be used within a ThemeProvider");
 
