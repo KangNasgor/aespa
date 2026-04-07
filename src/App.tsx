acd92ce8ee7fef1ffdb8ec2102de39b2de5a637d
Luxoretion <leal643201@gmail.com>
2026-04-07 18:14:02 +0700

    adadwawdawd

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ interface Members {
   name : string,
   image : string,
   description : string,
+  color : string,
 }
 
 const members : Members[] = [
@@ -17,25 +18,29 @@ const members : Members[] = [
     id: 1,
     name: 'KARINA',
     image: 'members/Karina.webp',
-    description : 'Karina (카리나 karina) was born on April 11, 2000 in Suwon-si, South Korea. She is the leader, a dancer, rapper and visual of the group aespa and a rapper and vocalist of the unit GOT the beat.'
+    description : 'Karina (카리나 karina) was born on April 11, 2000 in Suwon-si, South Korea. She is the leader, a dancer, rapper and visual of the group aespa and a rapper and vocalist of the unit GOT the beat.',
+    color : '#0d02fe'
   },
   {
     id: 2,
     name: 'GISELLE',
     image: 'members/Giselle.webp',
-    description : 'Giselle (지젤 jijel) was born on October 30, 2000 in Seoul, South Korea. She is a rapper and vocalist of the group aespa and ZOO unit.'
+    description : 'Giselle (지젤 jijel) was born on October 30, 2000 in Seoul, South Korea. She is a rapper and vocalist of the group aespa and ZOO unit.',
+    color : '#fc261a'
   },
   {
     id: 3,
     name: 'WINTER',
     image: 'members/Winter.webp',
-    description : 'Winter (윈터 winteo) was born on January 1, 2001 in Busan, South Korea. She is a vocalist, dancer and visual of the girl group aespa and a vocalist and the maknae of the unit GOT the beat.'
+    description : 'Winter (윈터 winteo) was born on January 1, 2001 in Busan, South Korea. She is a vocalist, dancer and visual of the girl group aespa and a vocalist and the maknae of the unit GOT the beat.',
+    color : '#01fe00'
   },
   {
     id: 4,
     name: 'NINGNING',
     image: 'members/Ningning.webp',
-    description : 'Ningning (닝닝 ningning) was born on October 23, 2002 in Heilongjiang, China. She is a vocalist and the maknae of the group aespa.'
+    description : 'Ningning (닝닝 ningning) was born on October 23, 2002 in Heilongjiang, China. She is a vocalist and the maknae of the group aespa.',
+    color : '#fcff00'
   }
 ]
 
@@ -45,6 +50,11 @@ function App() {
 
   const [imageClicked, setImageClicked] = useState<boolean>(false);
   const [memberImageClicked, setMemberImageClicked] = useState<boolean>(false)
+  const [currentMemberIndex, setCurrentMemberIndex] = useState<number | null>(0)
+  const imageMemberClicked = (index : number) => {
+    setCurrentMemberIndex(index);
+
+  }
   return (
     <>
       <motion.section id="hero-section" data-theme={theme} className="bg-[#070fc1] dark:bg-black w-full h-screen relative"
@@ -132,19 +142,51 @@ function App() {
       <section data-theme={theme} className="min-h-screen bg-[#070fc1] dark:bg-black py-20">
             <h1 className="font-bold text-5xl text-white mb-24 text-center">The <span className="text-[#f4f015]">Members</span></h1>
             <div className="flex flex-rows gap-10 w-9/12 mx-auto">
+                <AnimatePresence>
+                    {currentMemberIndex && (
+                      <motion.div
+                        initial={{ opacity : 0 }}
+                        animate={{ opacity : 1 }}
+                        exit={{ opacity : 0 }}
+                        onClick={() => setCurrentMemberIndex(null)}
+                        className="fixed inset-0 z-20 bg-black/70">
+                      </motion.div>
+                    )}
+                </AnimatePresence>
               {members.map((member, index) => (
-                <div key={member.name} className="flex w-6/12 relative justify-center">
-                    <motion.div layout className={`group ${member.id % 2 === 0 ? 'translate-y-10' : 'translate-y-0'} ${memberImageClicked === true ? 'fixed left-6/12 w-7/12 top-6/12 -translate-y-6/12 transform -translate-x-6/12 z-50' : 'relative'}`} onClick={() => setMemberImageClicked(prev => !prev)}>
-                      <span className="absolute -top-15 -left-5 text-8xl font-black opacity-10 select-none">
-                        0{index + 1}
-                      </span>
-                      <div data-name={member.name} className={`relative rounded-lg overflow-hidden after:absolute after:bg-linear-to-t after:from-black after:to-transparent after:w-full after:h-6/12 after:opacity-0 group-hover:after:opacity-75 after:bottom-0 after:left-0 after:transition after:overflow-hidden`} >
-                        <img src={member.image} className="w-full h-full object-cover rounded-lg" alt={member.name}/>
-                        <div className="absolute bottom-5 left-5 z-20 text-white opacity-0 group-hover:opacity-100">
-                          <h1 className="font-bold text-2xl">{member.name}</h1>
-                        </div>
+                <div key={member.name} className="w-full relative">
+                  <motion.div layoutId={member.name} className={`group cursor-pointer relative h-full w-full ${member.id % 2 === 0 ? 'translate-y-10' : 'translate-y-0'}`} onClick={() => setCurrentMemberIndex(member.id)}>
+                    <motion.span layout className={`absolute -top-15 -left-5 text-8xl font-black dark:text-white opacity-10 select-none ${currentMemberIndex === member.id ? 'hidden' : 'block'}`}>
+                      0{index + 1}
+                    </motion.span>
+                    <div className={`relative flex justify-center rounded-lg overflow-hidden after:absolute after:bg-linear-to-t after:from-black after:to-transparent after:w-full after:h-6/12 ${currentMemberIndex === member.id ? 'after:opacity-80 after:z-30' : 'after:opacity-0 group-hover:after:opacity-75'} after:bottom-0 after:left-0 after:transition after:overflow-hidden`} >
+                      <motion.img layoutId={member.image} src={member.image} className={`rounded-lg`} alt={member.name} />
+                      <div className={`absolute bottom-5 left-5 z-40 text-white ${currentMemberIndex === member.id ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}`}>
+                        <motion.h1 layout className="font-bold text-2xl">{member.name}</motion.h1>
                       </div>
-                    </motion.div>
+                    </div>
+                  </motion.div>
+                  {currentMemberIndex === member.id && (
+                    <AnimatePresence>
+                      <motion.div layout layoutId={member.name} className="fixed left-6/12 w-8/12 top-1/2 -translate-y-1/2 transform -translate-x-6/12 overflow-hidden z-50">
+                        <div style={{ backgroundColor: currentMemberIndex === member.id ? member.color : 'transparent' }} className={`relative flex justify-center rounded-lg overflow-hidden after:absolute after:bg-linear-to-t after:from-black after:to-transparent after:w-full after:h-6/12 ${currentMemberIndex === member.id ? 'after:opacity-80 after:z-30' : 'after:opacity-0 group-hover:after:opacity-75'} after:bottom-0 after:left-0 after:transition after:overflow-hidden`} >
+                          <motion.img layoutId={member.image} src={member.image} className="w-75 rounded-lg" alt={member.name} />
+                          <div className={`absolute bottom-5 left-5 z-40 text-white ${currentMemberIndex === member.id ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}`}>
+                            <motion.h1 layout className="font-bold text-2xl">{member.name}</motion.h1>
+                            <AnimatePresence>
+                              <motion.h3
+                                className={currentMemberIndex === member.id ? 'block' : 'hidden'}
+                                layout
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                              >{member.description}</motion.h3>
+                            </AnimatePresence>
+                          </div>
+                        </div>
+                      </motion.div>
+                    </AnimatePresence>
+                  )}
                 </div>
               ))}
             </div>
