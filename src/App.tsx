c958d154ca044300acd416c26372d8759dcdd460
Luxoretion <leal643201@gmail.com>
2026-04-17 16:55:00 +0700

    laehfgaifugawfuigawflu

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const members : Members[] = [
     id: 3,
     name: 'WINTER',
     image: 'members/Winter.webp',
-    description : 'Winter (윈터 winteo) was born on January 1, 2001 in Busan, South Korea. She is a vocalist, dancer and visual of the girl group aespa and a vocalist and the maknae of the unit GOT the beat.',
+    description : 'Winter (윈터 winteo) was born on January 1, 2001 in Busan, South Korea. She is a vocalist, dancer, and visual of the girl group aespa and also a vocalist and the maknae of the unit GOT the beat.',
     color : '#01fe00'
   },
   {
@@ -55,8 +55,8 @@ function App() {
   const [currentMemberIndex, setCurrentMemberIndex] = useState<number | null>(null);
 
   return (
-    <>
-      <motion.section id="hero-section" data-theme={theme} className="bg-[#070fc1] dark:bg-black w-full h-screen relative"
+    <main data-theme={theme}>
+      <motion.section id="hero-section" className="bg-[#070fc1] dark:bg-black w-full h-screen relative"
         initial={{
           filter: "blur(10px)",
         }}
@@ -105,12 +105,12 @@ function App() {
           </div>
         </div>
       </motion.section>
-      <section data-theme={theme} className="bg-[#070fc1] dark:bg-black min-h-screen flex items-center">
+      <section className="bg-[#070fc1] dark:bg-black min-h-screen flex items-center">
         <div className="flex flex-row justify-around items-center gap-5">
           <div className="w-5/12 flex flex-col gap-20">
             <h1 className="text-[#f4f015] dark:text-white font-bold text-5xl">The æ concept that changed everything</h1>
             <div>
-              <p className="text-[#f4f015] dark:text-white font-bold text-2xl">
+              <p className="text-white dark:text-white font-bold text-2xl">
                 {headingText} is a pioneering 4th generation kpop girl group under SM Entertainment.
                 The group name is a combination of "ae" (Avatar X Experience) and "aspect",
                 symbolizing the concept of a new experience by meeting a new world through another version of yourself.
@@ -138,7 +138,7 @@ function App() {
           </div>
         </div>
       </section>
-      <section data-theme={theme} className="min-h-screen bg-[#070fc1] dark:bg-black py-20">
+      <section className="min-h-screen bg-[#070fc1] dark:bg-black py-20">
             <h1 className="font-bold text-5xl text-white mb-24 text-center">The <span className="text-[#f4f015]">Members</span></h1>
             <div className="flex flex-rows gap-10 w-9/12 mx-auto">
                 <AnimatePresence>
@@ -170,15 +170,15 @@ function App() {
                         <motion.div layoutId={member.name} className="fixed left-6/12 w-8/12 top-1/2 -translate-y-1/2 transform -translate-x-6/12 overflow-hidden z-50">
                           <div style={{ backgroundColor: currentMemberIndex === member.id ? member.color + 'd9' : 'transparent' }} className={`relative flex justify-center rounded-lg overflow-hidden after:absolute after:bg-linear-to-t after:from-black after:to-transparent after:w-full after:h-6/12 ${currentMemberIndex === member.id ? 'after:opacity-80 after:z-30' : 'after:opacity-0 group-hover:after:opacity-75'} after:bottom-0 after:left-0 after:transition after:overflow-hidden`} >
                             <motion.img layoutId={member.image} src={member.image} className="w-75 rounded-lg" alt={member.name} />
-                            <div className={`absolute bottom-5 left-5 z-40 text-white ${currentMemberIndex === member.id ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}`}>
+                            <div className={`absolute bottom-5 left-5 px-3 z-40 text-white ${currentMemberIndex === member.id ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}`}>
                               <motion.h1 layout className="font-bold text-2xl">{member.name}</motion.h1>
                               <AnimatePresence>
                                 <motion.h3
-                                  className={currentMemberIndex === member.id ? 'block' : 'hidden'}
+                                  className={`${currentMemberIndex === member.id ? 'block' : 'hidden'} font-semibold`}
                                   layout
                                   initial={{ opacity: 0 }}
-                                  animate={{ opacity: 1 }}
-                                  exit={{ opacity: 0 }}
+                                  animate={{ opacity: 0.8 }}
+                                  exit={{ opacity: 0, transition : { duration : 0.5 }}}
                                 >{member.description}</motion.h3>
                               </AnimatePresence>
                             </div>
@@ -190,8 +190,20 @@ function App() {
               ))}
             </div>
       </section>
-      <section className="min-h-screen"></section>
-    </>
+      <section className="min-h-screen bg-[#070fc1] dark:bg-black py-20 px-10 relative">
+              <div className="w-full overflow-hidden">
+                  <h1 className="text-white text-center font-bold text-4xl">THE DEBUT : <span className="text-[#f4f015]">BLACK MAMBA</span></h1>
+              </div>
+              <div className="w-full my-10">
+                <div className="w-6/12 mx-auto">
+                  <img src="/images/aespa-blackmamba-img-1.webp" className=""/>
+                </div>
+              </div>
+              <div className="w-6/12 mx-auto">
+                <p className="text-white text-center font-semibold"> Released on November 17, 2020. Black Mamba is the debut and first digital single by South Korean girl group aespa. The music video reached 100 million views on January 8, 2021 and 200 million views on January 15, 2022. It was featured on their second mini album Girls as a digital bonus track on July 8, 2022.</p>
+              </div>
+      </section>
+    </main>
   );
 }
 
